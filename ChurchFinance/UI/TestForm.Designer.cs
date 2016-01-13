3b0158e5409783a4fc2662ca9480804742f23555
이길복 <pst1302@gmail.com>
2016-01-13 16:18:12 +0900

    neoTab 간소화중

--- ChurchFinance/UI/TestForm.Designer.cs
+++ ChurchFinance/UI/TestForm.Designer.cs
@@ -28,19 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.neoTabWindow1 = new NeoTabControlLibrary.NeoTabWindow();
+            this.neoTabPage1 = new NeoTabControlLibrary.NeoTabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.neoTabWindow1)).BeginInit();
+            this.neoTabWindow1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // neoTabWindow1
+            // 
+            this.neoTabWindow1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.neoTabWindow1.Controls.Add(this.neoTabPage1);
+            this.neoTabWindow1.Location = new System.Drawing.Point(12, 12);
+            this.neoTabWindow1.Name = "neoTabWindow1";
+            this.neoTabWindow1.RendererName = null;
+            this.neoTabWindow1.SelectedIndex = 0;
+            this.neoTabWindow1.Size = new System.Drawing.Size(762, 421);
+            this.neoTabWindow1.TabIndex = 0;
+            // 
+            // neoTabPage1
+            // 
+            this.neoTabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.neoTabPage1.Name = "neoTabPage1";
+            this.neoTabPage1.Text = "neoTabPage1";
+            this.neoTabPage1.ToolTipText = "neoTabPage1";
             // 
             // TestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(786, 455);
+            this.Controls.Add(this.neoTabWindow1);
             this.Name = "TestForm";
             this.Text = "TestForm";
+            ((System.ComponentModel.ISupportInitialize)(this.neoTabWindow1)).EndInit();
+            this.neoTabWindow1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private NeoTabControlLibrary.NeoTabWindow neoTabWindow1;
+        private NeoTabControlLibrary.NeoTabPage neoTabPage1;
     }
 }
--- ChurchFinance/UI/TestForm.cs
+++ ChurchFinance/UI/TestForm.cs
@@ -12,9 +12,23 @@ namespace UI
 {
     public partial class TestForm : Form
     {
+
+        private NeoTabControlLibrary.NeoTabPage neoTabPage2;
+
         public TestForm()
         {
             InitializeComponent();
+
+            neoTabWindow1.Renderer = NeoTabControlLibrary.AddInRendererManager.LoadRenderer("MarginBlueRendererVS2");
+
+            neoTabPage2 = new NeoTabControlLibrary.NeoTabPage();
+
+            neoTabPage2.BackColor = System.Drawing.Color.Transparent;
+            neoTabPage2.Name = "neoTabPage2";
+            neoTabPage2.Text = "neoTabPage2";
+            neoTabPage2.ToolTipText = "neoTabPage2";
+
+            neoTabWindow1.Controls.Add(neoTabPage2);
         }
     }
 }
