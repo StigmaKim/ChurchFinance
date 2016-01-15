56bac8c4f0184a41db7fca74c954714f41c10576
StigmaKim <jjang9c@naver.com>
2016-01-15 16:15:23 +0900

    Sqlite 2

--- ChurchFinance/UI/SQLite.cs
+++ ChurchFinance/UI/SQLite.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Diagnostics;
 
 namespace UI
 {
     class SQLite
     {
+        SQLiteConnection m_dbConnection;
+
+        public void ConnectToDB()
+        {
+            try
+            {
+                m_dbConnection = new SQLiteConnection("Data Source=TestDB.sqlite;Version=3;");
+                m_dbConnection.Open();
+                Debug.WriteLine("Oepn DB Connection");
+            }
+            catch(SQLiteException e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+
+        public void CloseDB()
+        {
+            try
+            {
+                m_dbConnection.Close();
+            }
+            catch(SQLiteException e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+
     }
 }
