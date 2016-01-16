4929b1e1b8a6f895a8aee1ff8f70280591600db6
StigmaKim <jjang9c@naver.com>
2016-01-17 00:13:11 +0900

    GoodGame Good JokBal

--- ChurchFinance/UI/Form1.Designer.cs
+++ ChurchFinance/UI/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.neoTabWindow1 = new NeoTabControlLibrary.NeoTabWindow();
-            this.imgBtnContainer1 = new UI.ImgBtnContainer();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -37,6 +36,7 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.panel5 = new System.Windows.Forms.Panel();
+            this.imgBtnContainer1 = new UI.ImgBtnContainer();
             ((System.ComponentModel.ISupportInitialize)(this.neoTabWindow1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -50,16 +50,8 @@
             this.neoTabWindow1.Location = new System.Drawing.Point(13, 177);
             this.neoTabWindow1.Name = "neoTabWindow1";
             this.neoTabWindow1.RendererName = null;
-            this.neoTabWindow1.Size = new System.Drawing.Size(993, 499);
+            this.neoTabWindow1.Size = new System.Drawing.Size(967, 499);
             this.neoTabWindow1.TabIndex = 1;
-            // 
-            // imgBtnContainer1
-            // 
-            this.imgBtnContainer1.BackColor = System.Drawing.SystemColors.Control;
-            this.imgBtnContainer1.Location = new System.Drawing.Point(13, 13);
-            this.imgBtnContainer1.Name = "imgBtnContainer1";
-            this.imgBtnContainer1.Size = new System.Drawing.Size(993, 97);
-            this.imgBtnContainer1.TabIndex = 2;
             // 
             // panel1
             // 
@@ -72,7 +64,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Location = new System.Drawing.Point(861, 6);
+            this.button1.Location = new System.Drawing.Point(843, 6);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(107, 40);
             this.button1.TabIndex = 0;
@@ -105,8 +97,8 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("굴림", 11F);
-            this.dateTimePicker1.Location = new System.Drawing.Point(767, 6);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Gulim", 11F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(750, 8);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 24);
             this.dateTimePicker1.TabIndex = 0;
@@ -118,12 +110,20 @@
             this.panel5.Size = new System.Drawing.Size(993, 2);
             this.panel5.TabIndex = 7;
             // 
+            // imgBtnContainer1
+            // 
+            this.imgBtnContainer1.BackColor = System.Drawing.SystemColors.Control;
+            this.imgBtnContainer1.Location = new System.Drawing.Point(13, 13);
+            this.imgBtnContainer1.Name = "imgBtnContainer1";
+            this.imgBtnContainer1.Size = new System.Drawing.Size(993, 97);
+            this.imgBtnContainer1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1013, 740);
+            this.ClientSize = new System.Drawing.Size(990, 740);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
