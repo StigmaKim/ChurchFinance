813d649a7b88d85c48311dbc79fbae030f5898dd
이길복 <pst1302@gmail.com>
2016-01-21 09:45:34 +0900

    되돌리기

--- ChurchFinance/UI/SpendDetail.cs
+++ ChurchFinance/UI/SpendDetail.cs
@@ -467,8 +467,8 @@ namespace UI
             DataView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             DataView.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
 
-            DataView.Font = new Font("Microsoft Sans Serif", 11);
-            DataView.RowTemplate.Height = 25;
+            DataView.Font = new Font("Microsoft Sans Serif", 12);
+            DataView.RowTemplate.Height = 30;
 
             // 컬럼 헤더
             DataView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
@@ -521,21 +521,11 @@ namespace UI
             pd = new PrintDocument();
             pgSettings = new PageSettings();
             pd.PrintPage += Pd_PrintPage;
-            printBtn.Click += PrintBtn_Click;
 
         }
 
         private void DataView_SelectionChanged(object sender, EventArgs e)
         {
-        }
-        
-        private void PrintBtn_Click(object sender, EventArgs e)
-        {
-            PrintPreviewDialog dig = new PrintPreviewDialog();
-
-            dig.Document = pd;
-
-            dig.ShowDialog();
         }
 
         #region 프린터 인쇄 처리
@@ -547,19 +537,13 @@ namespace UI
         /// <param name="e"></param>
         private void Pd_PrintPage(object sender, PrintPageEventArgs e)
         {
-
-            int curYPos;
-
-            SizeF sz = e.Graphics.MeasureString(label1.Text, new Font("Tahoma", 20));
-            //e.Graphics.DrawString(title.Text, new Font("Tahoma", 20), new SolidBrush(Color.Black), new Point((int)(pgSettings.PaperSize.Width / 2 - (sz.Width / 2)), curYPos));
-
             Bitmap tapBM = new Bitmap(pgSettings.PaperSize.Width, pgSettings.PaperSize.Height);
 
             // 사이즈 임시 저장
             Size tempSz = DataView.Size;
 
             // 높이에 맞는 사이즈 설정
-            DataView.Size = new Size(tempSz.Width, pgSettings.PaperSize.Height);
+            DataView.Size = new Size(tempSz.Width, 875);
 
             DataView.DrawToBitmap(tapBM, new Rectangle(new Point(0, 0), new Size(pgSettings.PaperSize.Width, pgSettings.PaperSize.Height)));
 
@@ -568,6 +552,14 @@ namespace UI
             DataView.Size = tempSz;
         }
 
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            PrintPreviewDialog dig = new PrintPreviewDialog();
+
+            dig.Document = pd;
+
+            dig.ShowDialog();
+        }
 
         #endregion
 
@@ -798,7 +790,7 @@ namespace UI
         private void SpendDetail_Paint(object sender, PaintEventArgs e)
         {
             label1.Text = date.Year + "년 " + date.Month + "월 지출 세부 명세";
-            label1.Location = new Point(350, 20);
+            label1.Location = new Point(350, 10);
             inputData();
         }
     }
